6251d068f21892bc8fb1be62031480589826470c
Albert Marques <albertmarques@live.com>
2024-11-13 03:36:21 -0300

    componentizando a API

---
+++ src/components/Formulario.vue
@@ -0,0 +1,29 @@
+<script setup>
+    const props = defineProps(['cadastraTarefa', 'tarefaTemporaria', 'trocarFiltro', 'editaTarefaTemp'])
+</script>
+
+<template>
+    <form @submit.prevent="cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input 
+                :value="tarefaTemporaria" 
+                @change="editaTarefaTemp" 
+                required 
+                type="text"
+                    placeholder="Digite aqui a descrição da tarefa" 
+                    class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="trocarFiltro" class="form-control">
+                    <option value="todas">Todas tarefas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
